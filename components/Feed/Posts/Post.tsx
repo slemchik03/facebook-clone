0237a8ada1296fdd56fcd09ea39843d51072296b
slemchik_03 <vadimmaster228@gmail.com>
2022-07-22 13:52:19 +0300

    a

--- components/Feed/Posts/Post.tsx
+++ components/Feed/Posts/Post.tsx
@@ -1,16 +1,9 @@
-<<<<<<< Updated upstream
-import { Timestamp } from "firebase/firestore";
-import { useRouter } from "next/router";
-import { FC, useEffect, useState } from "react";
-import PostItem from "./PostItem";
-import PostModal from "./PostModal";
-=======
 import { Timestamp } from "firebase/firestore"
 import { useRouter } from "next/router"
 import { FC, useEffect, useState } from "react"
 import PostItem from "./PostItem/PostItem"
 import PostModal from "./PostModal/PostModal"
->>>>>>> Stashed changes
+
 
 
 
@@ -24,20 +17,6 @@ export interface IPost {
     timestamp: Timestamp
 }
 
-<<<<<<< Updated upstream
-
-const Post: FC<Props> = (props) => {
-    const {
-        id
-    } = props
-
-    const [isModal, setModal] = useState(false)
-    const router = useRouter()
-
-    const closeHandler = () => {
-        router.query = {}
-        router.push("/", null, { shallow: true })
-=======
 interface Props extends IPost {
 
 }
@@ -48,35 +27,18 @@ const Post: FC<Props> = (props) => {
 
     const closeModalHandler = () => {
         router.push(router.route, null, { shallow: true })
->>>>>>> Stashed changes
-
         setModal(false)
     }
-
     useEffect(() => {
-<<<<<<< Updated upstream
-        if (router.query.post === id) {
-=======
         if (router.query.post === props.id) {
->>>>>>> Stashed changes
             setModal(true)
         }
     }, [router.query])
 
-<<<<<<< Updated upstream
-
-    return (
-        <div>
-            {
-                isModal ? <PostModal {...props} closeHandler={closeHandler} /> : <PostItem {...props} />
-            }
-        </div>
-=======
     return (
         <>
             {isModal ? <PostModal {...props} closeHandler={closeModalHandler} /> : <PostItem {...props} />}
         </>
->>>>>>> Stashed changes
     )
 }
 
